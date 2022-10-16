c15df726e60bd1fed2c2632044d3a1267d24867d
JiaboYao <897492023@qq.com>
2022-10-16 18:27:11 +0800

    add ycsbc

---
+++ ycsbc/db/db_factory.cc
@@ -0,0 +1,27 @@
+//
+//  basic_db.cc
+//  YCSB-C
+//
+//  Created by Jinglei Ren on 12/17/14.
+//  Copyright (c) 2014 Jinglei Ren <jinglei@ren.systems>.
+//
+
+#include "db/db_factory.h"
+
+#include <string>
+#include "db/basic_db.h"
+#include "db/leveldb_db.h"
+
+using namespace std;
+using ycsbc::DB;
+using ycsbc::DBFactory;
+
+DB* DBFactory::CreateDB(utils::Properties &props) {
+  if (props["dbname"] == "basic") {
+    return new BasicDB;
+  } else if (props["dbname"] == "leveldb") {
+    std::string dbpath = props.GetProperty("dbpath","/tmp/test-leveldb");
+    return new LevelDB(dbpath.c_str(), props);
+  } else return NULL;
+}
+
---
+++ ycsbc/ycsb_build_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Please replace the content in quotation marks according to your environment
+set CPLUS_INCLUDE_PATH="the absolute path of MioDB/include":$CPLUS_INCLUDE_PATH
+set LIBRARY_PATH="the absolute path of MioDB/build":$LIBRARY_PATH
