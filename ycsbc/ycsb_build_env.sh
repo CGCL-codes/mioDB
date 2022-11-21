6f796f2bdec65832a2bb8f881194d1df9d6d1d5a
JiaboYao <897492023@qq.com>
2022-11-21 22:58:43 +0800

    update readme

--- ycsbc/ycsb_build_env.sh
+++ ycsbc/ycsb_build_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Please replace the content in quotation marks according to your environment
-set CPLUS_INCLUDE_PATH="the absolute path of MioDB/include":$CPLUS_INCLUDE_PATH
-set LIBRARY_PATH="the absolute path of MioDB/build":$LIBRARY_PATH
+export CPLUS_INCLUDE_PATH="the absolute path of MioDB/include":$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH="the absolute path of MioDB/build":$LIBRARY_PATH
